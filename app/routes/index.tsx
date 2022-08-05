6e76de9522b4eab07f6efae88193401582fe5fa9
michael heller <michaelheller007@googlemail.com>
2022-08-06 00:02:29 +0200

    add boat requests

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,41 @@
+import { useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from '@remix-run/node'
+
+export const loader: LoaderFunction = async ({request}) => {
+  const timeframe = [60, 525600];
+  const time = timeframe.join(',');
+  const lat = 51.3023
+  const lng = 6.7355
+  const url = new URL(request.url);
+  const distance = url.searchParams.get('distance') || 2
+  const res = await fetch(`https://www.marinetraffic.com/en/reports?asset_type=vessels&columns=time_of_latest_position:desc,
+  flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_
+  live_map,area,lat_of_latest_position,lon_of_latest_position&time_of_latest_position_between=${time}&near_me=${lat},${lng},${distance}`, {
+    headers: {
+      'vessel-image': '0053e92efe9e7772299d24de2d0985adea14',
+    },
+
+  });
+  const data = await res.json();
+  return data
+};
+
 export default function Index() {
+  const {data} = useLoaderData()
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+        <h1>Welcome to Remix</h1>
+        <ul>
+          {data.map((vessel:any) => (
+              <li key={vessel.SHIPNAME}>
+                <div style={{}}>
+                  <img className='list_photos_a_img' title='Photos of: RHEINPOESIE' alt='RHEINPOESIE'
+                       src={`//photos.marinetraffic.com/ais/showphoto.aspx?shipid=${vessel.SHIP_ID}&size=thumb`}></img>
+                  <div>{vessel.SHIPNAME}</div>
+                </div>
+              </li>
+          ))}
+        </ul>
+      </div>
   );
 }
